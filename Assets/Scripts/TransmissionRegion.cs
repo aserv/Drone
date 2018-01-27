dd7443a2a2d34d23285a6cf7c727a4c4ac1c3c24
aserv <alexs9@vt.edu>
2018-01-27 15:20:34 -0500

    Add Terminal Zone Toggles

--- Assets/Scripts/TransmissionRegion.cs
+++ Assets/Scripts/TransmissionRegion.cs
@@ -2,19 +2,23 @@
 
 [RequireComponent(typeof(Collider))]
 public class TransmissionRegion : MonoBehaviour {
+    private bool m_Active;
+
     void OnTriggerEnter(Collider col) {
-        Debug.Log("Enter");
-        Debug.Log(col);
         if (col.CompareTag("Player")) {
             col.GetComponent<PlayerDroneControl>().SetCommandState(true);
         }
     }
 
     void OnTriggerExit(Collider col) {
-        Debug.Log("Exit");
-        Debug.Log(col);
         if (col.CompareTag("Player")) {
             col.GetComponent<PlayerDroneControl>().SetCommandState(false);
         }
+    }
+
+    public void Activate(bool val) {
+        m_Active = val;
+        GetComponent<SpriteRenderer>().color = m_Active ? Color.white : Color.gray;
+        GetComponent<Collider>().enabled = m_Active;
     }
 }
