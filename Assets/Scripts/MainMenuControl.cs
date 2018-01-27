6748aa168dbff2395d0f0ca9f645aef24ffd4b1f
Masterfishy <iamzachlong@gmail.com>
2018-01-27 16:05:29 -0500

    Zach - 4:06
    Added Controls and Credits menus.
    Added Fonts folder

--- Assets/Scripts/MainMenuControl.cs
+++ Assets/Scripts/MainMenuControl.cs
@@ -5,11 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuControl : MonoBehaviour {
 
+    public GameObject ControlsPanel;
+    public GameObject CreditsPanel;
+
+    private bool control = false;
+    private bool credits = false;
+
     public void clickPlay() {
         SceneManager.LoadScene(1);
     }
 
+    public void clickControls() {
+        if (control) {
+            control = false;
+            ControlsPanel.SetActive(false);
+        } else {
+            control = true;
+            ControlsPanel.SetActive(true);
+        }
+    }
+
     public void clickQuit() {
         Application.Quit();
+    }
+
+    public void clickCredits() {
+        if (credits) {
+            credits = false;
+            CreditsPanel.SetActive(false);
+        } else {
+            credits = true;
+            CreditsPanel.SetActive(true);
+        }
     }
 }
